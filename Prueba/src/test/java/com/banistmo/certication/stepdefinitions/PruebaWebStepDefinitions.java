e8eb7cc1a0980c4b56805a0d644836f530463b02
juantobo <juan.p.tobon@banistmo.com>
2024-05-16 14:46:16 -0500

    Ajuste build.gradle eliminando dependencia de JUnit y aplicando el Test Suite para que estas dependencias se administren automaticamente

--- Prueba/src/test/java/com/banistmo/certication/stepdefinitions/PruebaWebStepDefinitions.java
+++ Prueba/src/test/java/com/banistmo/certication/stepdefinitions/PruebaWebStepDefinitions.java
@@ -7,22 +7,31 @@ import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Open;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
+import net.serenitybdd.screenplay.matchers.WebElementStateMatchers;
+import net.serenitybdd.screenplay.waits.WaitUntil;
 
 public class PruebaWebStepDefinitions {
 
     @Before
-    public void setStage(){
+    public void setStage() {
         OnStage.setTheStage(new OnlineCast());
     }
 
     @Given("The user Open the page")
     public void theUserOpenThePage() {
-       OnStage.theActorCalled("User").wasAbleTo(Open.browserOn().thePageNamed("pages.urlTest"));
+        OnStage.theActorCalled("User").wasAbleTo(Open.browserOn().thePageNamed("pages.urlTest"));
     }
+
     @When("The user fill the forms")
     public void theUserFillTheForms() {
         OnStage.theActorInTheSpotlight().attemptsTo(
+                WaitUntil.the(".unauthenticated-nav-bar__sign-up", WebElementStateMatchers.isClickable()).forNoMoreThan(10).seconds(),
                 Click.on(".unauthenticated-nav-bar__sign-up")
         );
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
